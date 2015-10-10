93717aad79a0dfdd28cd7584657148fcc92cc877
sempeck <jarek.sepek@mediorama.pl>
2015-10-10 19:45:25 +0200

    delete some comments

--- choinka.pp
+++ choinka.pp
@@ -40,21 +40,6 @@ procedure left(x:Integer);
   end;
 
 
-
-   // 10 razy spacja oraz 1 '*'
-   //  9 razy spacja oraz 2 '*'
-   //  8 razy spacja oraz 3 '*'
-   //  7 razy spacja oraz 4 '*'
-   //  6 razy spacja oraz 5 '*'
-   //  5 razy spacja oraz 6 '*'
-   //  4 razy spacja oraz 7 '*'
-   //  3 razy spacja oraz 8 '*'
-   //  2 razy spacja oraz 9 '*'
-   //  1 razy spacja oraz 10 '*'
-
-// odejmij 1 od spacja oraz dodaj 1 do '*'
-// odejmuj od spacja do 0, dodawaj do '*'
-
 begin
   
   // choinka(20);
